953c7002b72472ea108c5a2e415f2720ea5a6316
Admin123 <ghislaincabrel.kemfang@gmail.com>
2018-01-31 09:49:27 +0100

    Building_a_RESTful_Web_Service

---
+++ Building_a_RESTful_Web_Service/src/main/java/com/example/demo/BuildingAResTfulWebServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingAResTfulWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingAResTfulWebServiceApplication.class, args);
+	}
+}
---
+++ Building_a_RESTful_Web_Service/src/main/java/com/example/demo/Greeting_Controller.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class Greeting_Controller {
+	private String template = "hello , %s!";
+	private AtomicLong counter = new AtomicLong();
+	
+	@CrossOrigin(origins = "*" )
+	@RequestMapping("/greeting")
+	public Greeting greeting (@RequestParam(value="name",defaultValue="world") String name) {
+		return new Greeting (counter.incrementAndGet(),String.format(template,name));
+	}
+
+}
